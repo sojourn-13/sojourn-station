fcd1af850351ed9345d071e4234f9c7c4cd8742b
Dissentbay <chapolin_rox@hotmail.com>
2023-03-06 19:47:16 +0000

    i can't edit .js like I want to

--- tgui/packages/tgui/interfaces/Cryo.js
+++ tgui/packages/tgui/interfaces/Cryo.js
@@ -84,7 +84,7 @@ const CryoContent = (props, context) => {
           <LabeledList.Item label="Temperature">
             <AnimatedNumber value={data.cellTemperature} /> K
           </LabeledList.Item>
-          <LabeledList.Item label="Eject">
+          <LabeledList.Item label="Door">
             <Button
               icon={data.isOpen ? 'unlock' : 'lock'}
               onClick={() => act('door')}
