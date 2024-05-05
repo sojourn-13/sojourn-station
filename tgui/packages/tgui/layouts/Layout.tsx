49a8a0ac19a90998bd0d1fab7b7b65cb8cccc3fd
ShadowLarkens <shadowlarkens@gmail.com>
2024-05-05 11:58:40 -0700

    Fix linter error and vending UI

--- tgui/packages/tgui/layouts/Layout.tsx
+++ tgui/packages/tgui/layouts/Layout.tsx
@@ -7,13 +7,13 @@
 import { classes } from 'common/react';
 import { useEffect, useRef } from 'react';
 
+import { useBackend } from '../backend';
 import {
   BoxProps,
   computeBoxClassName,
   computeBoxProps,
 } from '../components/Box';
 import { addScrollableNode, removeScrollableNode } from '../events';
-import { useBackend } from '../backend';
 
 type Props = Partial<{
   theme: string;
