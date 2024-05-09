d9709c9038742eae4a6029f3944905c6734386cf
ShadowLarkens <shadowlarkens@gmail.com>
2024-05-08 21:05:57 -0700

    Replace join game dialog

--- tgui/packages/tgui/interfaces/CrewManifest.tsx
+++ tgui/packages/tgui/interfaces/CrewManifest.tsx
@@ -2,6 +2,7 @@ import { useBackend } from 'tgui/backend'
 import { Section, Table } from 'tgui/components'
 import { Window } from 'tgui/layouts'
 import { classes } from '../../common/react'
+import { departmentData } from './common/departments'
 
 type CrewMember = {
   name: string
@@ -88,19 +89,4 @@ export const CrewManifest = props => {
       </Window.Content>
     </Window>
   )
-}
-
-const departmentData = {
-  heads: { name: 'Command Staff' },
-  sec: { name: 'Security - Marshals' },
-  bls: { name: 'Security - Blackshield' },
-  med: { name: 'Soteria Medical' },
-  sci: { name: 'Soteria Research' },
-  chr: { name: 'Church of the Absolute' },
-  sup: { name: 'Lonestar Shipping Solutions' },
-  eng: { name: 'Artificers Guild' },
-  pro: { name: 'Prospector' },
-  civ: { name: 'Civilian' },
-  bot: { name: 'Silicon' },
-  ldg: { name: 'Lodge' }
 }
