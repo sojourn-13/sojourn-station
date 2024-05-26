discordia-space/CEV-Eris

[BOUNTY] MELEE REWORK! (#7780)
* Bring back the past

* spears can't broadstrike

* ui

* Fixes and indicators

* push attack

* fixes

* intervention attacks

* distant attacks

* human shield grabfu

* fixes part 3 the fixes strike back electric boogaloo

* fxi + grab stuff

* Update human_defense.dm

* Apply suggestions from code review

Co-authored-by: Pink-Chink <95178278+Pink-Chink@users.noreply.github.com>

* Update melee.dm

* Update item_attack.dm

* Update item_attack.dm

* Update item_attack.dm

* Update living_defense.dm

* Update item_attack.dm

* Update item_attack.dm

* last one?

* yep

* Update alerts.dmi

Co-authored-by: Pink-Chink <95178278+Pink-Chink@users.noreply.github.com>

code/_onclick/click.dm
@@ -163,8 +163,11 @@
 		if(A.Adjacent(src)) // see adjacent.dm
 			if(W)
 				// Return 1 in attackby() to prevent afterattack() effects (when safely moving items for example)
-				var/resolved = (SEND_SIGNAL(W, COMSIG_IATTACK, A, src, params)) || (SEND_SIGNAL(A, COMSIG_ATTACKBY, W, src, params)) || W.resolve_attackby(A, src, params)
+				var/resolved = (SEND_SIGNAL(W, COMSIG_IATTACK, A, src, params)) || (SEND_SIGNAL(A, COMSIG_ATTACKBY, W, src, params))
 				if(!resolved && A && W)
+					if(W.double_tact(src))
+						W.resolve_attackby(A, src, params)
+				else
 					W.afterattack(A, src, 1, params) // 1: clicking something Adjacent
 			else
 				if(ismob(A)) // No instant mob attacking
@@ -173,7 +176,8 @@
 			return
 		else // non-adjacent click
 			if(W)
-				W.afterattack(A, src, 0, params) // 0: not Adjacent
+				if(W.double_tact(src))
+					W.afterattack(A, src, 0, params) // 0: not Adjacent
 			else
 				setClickCooldown(DEFAULT_ATTACK_COOLDOWN) // no ranged spam
 				RangedAttack(A, params)

sojourn-13/sojourn-station

code/_onclick/click.dm
@@ -159,8 +159,11 @@
 		if(A.Adjacent(src)) // see adjacent.dm
 			if(W)
 				// Return 1 in attackby() to prevent afterattack() effects (when safely moving items for example)
-				var/resolved = (LEGACY_SEND_SIGNAL(W, COMSIG_IATTACK, A, src, params)) || (LEGACY_SEND_SIGNAL(A, COMSIG_ATTACKBY, W, src, params)) || W.resolve_attackby(A, src, params)
+				var/resolved = (LEGACY_SEND_SIGNAL(W, COMSIG_IATTACK, A, src, params)) || (LEGACY_SEND_SIGNAL(A, COMSIG_ATTACKBY, W, src, params))
 				if(!resolved && A && W)
+					if(W.double_tact(src))
+						W.resolve_attackby(A, src, params)
+				else
 					W.afterattack(A, src, 1, params) // 1: clicking something Adjacent
 			else
 				if(ismob(A)) // No instant mob attacking
@@ -169,7 +172,8 @@
 			return
 		else // non-adjacent click
 			if(W)
-				W.afterattack(A, src, 0, params) // 0: not Adjacent
+				if(W.double_tact(src))
+					W.afterattack(A, src, 0, params) // 0: not Adjacent
 			else
 				setClickCooldown(DEFAULT_ATTACK_COOLDOWN) // no ranged spam
 				RangedAttack(A, params)