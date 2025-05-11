86fbd9df4aa0e41cbfbc5f5ec19af90a3976d209
Trilby <trilbyspaseclone@gmail.com>
2025-05-11 14:50:22 -0400

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -8,17 +8,17 @@ export BYOND_MAJOR=515
 export BYOND_MINOR=1640
 
 #rust_g git tag
-export RUST_G_VERSION=3.2.0
+export RUST_G_VERSION=3.8.0
 
 #node version
 export NODE_VERSION=20
 export NODE_VERSION_PRECISE=20.12.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.1
+export SPACEMAN_DMM_VERSION=suite-1.1.9
 
 # Extools git tag
 export EXTOOLS_VERSION=v0.0.7
 
 # Python version for mapmerge and other tools
-export PYTHON_VERSION=3.7.9
+export PYTHON_VERSION=3.11.2
