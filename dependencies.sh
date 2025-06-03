ba8636e6916137fcc816a6a2f21960c3c45512b3
Trilbyspaceclone <30435998+Trilbyspaceclone@users.noreply.github.com>
2025-06-03 16:11:04 -0400

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -15,7 +15,7 @@ export NODE_VERSION=20
 export NODE_VERSION_PRECISE=20.12.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.1.9
+export SPACEMAN_DMM_VERSION=suite-1.7.1
 
 # Extools git tag
 export EXTOOLS_VERSION=v0.0.7
