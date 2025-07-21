6087ea8c00665cc4dc639edae5d25865e2fa385d
Trilby <trilbyspaseclone@gmail.com>
2025-07-21 18:09:47 -0400

    fixes maybe linter

--- tgui/packages/tgui-panel/panelFocus.ts
+++ tgui/packages/tgui-panel/panelFocus.ts
@@ -7,9 +7,9 @@
  * @license MIT
  */
 
-import { focusMap } from 'tgui/focus';
 import { canStealFocus, globalEvents } from 'tgui/events';
 import { vecLength, vecSubtract } from 'common/vector';
+import { focusMap } from 'tgui/focus';
 
 // Empyrically determined number for the smallest possible
 // text you can select with the mouse.
